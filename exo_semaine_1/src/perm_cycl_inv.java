568d7764a4d1e28ea5ca50bd83c55e75e598bd2f
gregverp <1107grverpalen@student.vinci.be>
2022-02-04 10:22:25 +0100

    5eme exo

---
+++ exo_semaine_1/src/perm_cycl_inv.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class perm_cycl_inv {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entrez 4 entiers.");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+        int tmp = a;
+        a = b;
+        b = c;
+        c = d;
+        d = tmp;
+        System.out.println(a+" "+b+" "+c+" "+d);
+
+    }
+}
