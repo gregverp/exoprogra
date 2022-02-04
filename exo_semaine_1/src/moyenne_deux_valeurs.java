c46715ecb81059a36fbf1090bd89303024b4f0a3
gregverp <1107grverpalen@student.vinci.be>
2022-02-04 09:45:49 +0100

    1er exo

---
+++ exo_semaine_1/src/moyenne_deux_valeurs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class moyenne_deux_valeurs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entrez deux entiers.");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int resultat = (a+b)/2;
+        System.out.println("Votre resultat est " + resultat + ".");
+    }
+}
