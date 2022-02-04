e17f381b3a1508ead0b9dcd54685165dfe546650
gregverp <1107grverpalen@student.vinci.be>
2022-02-04 09:49:57 +0100

    3eme exo

---
+++ exo_semaine_1/src/inversion_valeurs.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class inversion_valeurs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entrez 3 valeurs.");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int tmp = a;
+        a = c;
+        c = tmp;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
