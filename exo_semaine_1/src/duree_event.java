c1e7221804badd15f3d2c374a2ac4041ef879e45
gregverp <1107grverpalen@student.vinci.be>
2022-02-04 10:35:42 +0100

    6eme exo

---
+++ exo_semaine_1/src/duree_event.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class duree_event {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Quelle heure etait-il au lancement de l'evenement? Entre d'abord l'heure puis les minutes.");
+        int h = scan.nextInt();
+        int m = scan.nextInt();
+        System.out.println("Quelle heure etait-il à la fin de l'evenement? Entre d'abord l'heure puis les minuteS.");
+        int h2 = scan.nextInt();
+        int m2 = scan.nextInt();
+        int temps1 = h*60+m;
+        int temps2 = h2*60+m2;
+        int tempsTotal = temps2-temps1;
+        int mTotal = tempsTotal%60;
+        int hTotal = tempsTotal/60;
+        System.out.println("L'evenement a dure "+hTotal+"h"+mTotal);
+    }
+}
