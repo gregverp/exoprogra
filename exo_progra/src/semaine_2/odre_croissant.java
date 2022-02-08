e1a8aa56d5ac788d4a1be02d95acafdcd1076f77
localastro <1107grverpalen@student.epfc.eu>
2022-02-08 12:10:00 +0100

    amélioriation ordre croissant

--- exo_progra/src/semaine_2/odre_croissant.java
+++ exo_progra/src/semaine_2/odre_croissant.java
@@ -9,20 +9,21 @@ public class odre_croissant {
         double a = scan.nextDouble();
         double b = scan.nextDouble();
         double c = scan.nextDouble();
-        if (a > b)
-            if (b > c)
-                System.out.println(c + " " + b + " " + a);
-            else
-                System.out.println(b + " " + c + " " + a);
-        if (b > a)
-            if (a > c)
-                System.out.println(c + " " + a + " " + b);
-            else
-                System.out.println(a + " " + c + " " + b);
-        if (c > a)
-            if (a > b)
-                System.out.println(b + " " + a + " " + c);
-            else
-                System.out.println(a + " " + b + " " + c);
+        if (a > b){
+            double tmp = a;
+            a =b;
+            b =tmp;
+        }
+        if(b > c){
+            double tmp = b;
+            b = c;
+            c = tmp;
+        }
+        if(a>b){
+            double tmp = a;
+            a =b;
+            b=tmp;
+        }
+        System.out.println(a + " " + b + " " + "c");
     }
 }
