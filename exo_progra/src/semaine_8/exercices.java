d29000764cb9b0c7f0cc055e539a33a805912862
localastro <1107grverpalen@student.epfc.eu>
2022-03-18 13:13:07 +0100

    corrections

--- exo_progra/src/semaine_8/exercices.java
+++ exo_progra/src/semaine_8/exercices.java
@@ -37,20 +37,20 @@ public class exercices {
     }
 
     public static double moyenne(SeqInt s) {
-        if(estVide(s)) throw new IllegalArgumentException("Pas de moyenne sur une séquence vide.");
+        if (estVide(s)) throw new IllegalArgumentException("Pas de moyenne sur une séquence vide.");
         SeqIntIterator it = s.iterator();
         int somme = 0, counter = 0;
-        while(it.hasNext()){
+        while (it.hasNext()) {
             somme += it.next();
             counter++;
         }
-        return somme/(double)counter;
+        return somme / (double) counter;
     }
 
     public static int maximum(SeqInt s) {
         SeqIntIterator it = s.iterator();
-        if(estVide(s)) throw new IllegalArgumentException("Pas de max dans une séquence vide.");
-        int maximum = Integer.MIN_VALUE;
+        if (estVide(s)) throw new IllegalArgumentException("Pas de max dans une séquence vide.");
+        int maximum = it.next();
         while (it.hasNext()) {
             int current = it.next();
             if (current > maximum) maximum = current;
@@ -59,7 +59,7 @@ public class exercices {
     }
 
     public static int position(SeqInt s, int n) {
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
         int position = -1, counter = 0;
         while (it.hasNext()) {
@@ -71,130 +71,138 @@ public class exercices {
     }
 
     public static int premierePosition(SeqInt s, int n) {
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
         int position = -1, counter = 0;
-        while (it.hasNext()) {
+        while (it.hasNext() && position == -1) {
             counter++;
-            if (position == -1 && n == it.next()) position = counter;
+            if (n == it.next()) position = counter;
+
         }
         return position;
     }
 
     public static boolean estCroissante(SeqInt s) {
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
         boolean returnable = true;
-        int current, previous  = 0;
-        while (it.hasNext()) {
-           current = it.next();
-           if(previous > current) returnable = false;
-           previous = current;
+        if (it.hasNext()) {
+            int previous = it.next();
+            while (it.hasNext() && returnable == true) {
+                int current = it.next();
+                if (previous > current) returnable = false;
+            }
         }
         return returnable;
     }
 
-    public static boolean possedeDoublon(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static boolean possedeDoublon(SeqInt s) {
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
         boolean returnable = false;
-        int current, previous = 0;
-        while(it.hasNext()){
-            current = it.next();
-            if (previous == current) returnable = true;
-            previous = current;
+        if (it.hasNext()) {
+            int current = it.next();
+            while (it.hasNext() && !returnable) {
+                int previous = it.next();
+                if (previous == current) returnable = true;
+            }
         }
         return returnable;
     }
 
-    public static int positionMax(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static int positionMax(SeqInt s) {
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
-        int max = maximum(s), counter = 0, position = -1;
-        while(it.hasNext()){
-            counter++;
-            if(it.next() == max){
-                position = counter;
+        if(it.hasNext()) {
+            int max = it.next(), counter = 1, position = 1;
+            while (it.hasNext()) {
+                int current = it.next();
+                if (current >= max) {
+                    max = current;
+                    position = counter;
+                }
+                counter++;
             }
         }
         return position;
     }
 
-    public static int premierePositionMax(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static int premierePositionMax(SeqInt s) {
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
-        int max = maximum(s), counter = 0, position = -1;
-        while(it.hasNext()){
-            counter++;
-            if(position == -1 && it.next() == max){
-                position = counter;
+        if(it.hasNext()) {
+            int max = it.next(), counter = 1, position = -1;
+            while (it.hasNext()) {
+                int current = it.next();
+                if (current > max) {
+                    max = current;
+                    position = counter;
+                }
+                counter++;
             }
         }
         return position;
     }
 
-    public static int quantiteDeMax(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static int quantiteDeMax(SeqInt s) {
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
-        int max = maximum(s), counter = 0;
-        while(it.hasNext()){
-            if(it.next() == max) counter++;
+        if(it.hasNext()) {
+            int max = it.next(), counter = 1;
+            while (it.hasNext()) {
+                int current = it.next();
+                if (current > max) {
+                    max = current;
+                    counter = 1;
+                }
+                if (current == max) counter++;
+            }
         }
-
         return counter;
     }
 
-    public static int plusGrandNegatif(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static int plusGrandNegatif(SeqInt s) {
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
-        int smallest = Integer.MAX_VALUE, current;
-        while(it.hasNext()){
+        int smallest = it.next(), current;
+        while (it.hasNext()) {
             current = it.next();
-            if(current < 0 && current < smallest) smallest = current;
+            if (current < 0 && current < smallest) smallest = current;
         }
 
         return smallest;
     }
 
-    public static SeqInt boundaries(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static SeqInt boundaries(SeqInt s) {
+        if (estVide(s)) throw new IllegalArgumentException("Séquence vide.");
         SeqIntIterator it = s.iterator();
-        int smallest = Integer.MAX_VALUE, highest = Integer.MIN_VALUE;
-        while(it.hasNext()){
-            int current = it.next();
-            if(current < smallest) smallest = current;
-            if(current > highest) highest = current;
+        int current = it.next();
+        int smallest = current, highest = current;
+        while (it.hasNext()) {
+            current = it.next();
+            if (current < smallest) smallest = current;
+            if (current > highest) highest = current;
         }
         return new SeqInt(smallest, highest);
     }
 
-    public static SeqInt twoHighest(SeqInt s){
-        if(estVide(s)) throw new IllegalArgumentException("Séquence vide.");
+    public static SeqInt twoHighest(SeqInt s) {
+        if (taille(s) < 2) throw new IllegalArgumentException("Séquence avec moins de 2 nombres.");
         SeqIntIterator it = s.iterator();
-        int second = Integer.MIN_VALUE, first = Integer.MIN_VALUE;
-        if(taille(s) < 2) return null;
-        while(it.hasNext()){
-            int current = it.next();
-            if(current > first) first = current;
-            if(current != first && current > second) second = current;
+        int current = it.next(), first = current, second = 0;
+        if(taille(s) == 2){
+            if(current == it.next()) throw new IllegalArgumentException("La séquence possède deux nombres identiques seulement.");
+        }
+        while (it.hasNext()) {
+            current = it.next();
+            if (current > first)  first = current;
+            if (current != first && current > second) second = current;
         }
         return new SeqInt(first, second);
     }
 
     public static void main(String[] args) {
-        SeqInt s = new SeqInt(7, 45, -7);
-        System.out.println(somme(s));
-        System.out.println(quantiteNombre(s, 14));
-        System.out.println(position(s, 7));
-        System.out.println(premierePosition(s, 7));
-        System.out.println(estCroissante(s));
-        System.out.println(possedeDoublon(s));
-        System.out.println(positionMax(s));
-        System.out.println(premierePositionMax(s));
-        System.out.println(quantiteDeMax(s));
-        System.out.println(plusGrandNegatif(s));
-        System.out.println(boundaries(s));
+        SeqInt s = new SeqInt(7, 7);
         System.out.println(twoHighest(s));
-        System.out.println(moyenne(s));
     }
 }
